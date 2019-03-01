1736213605b16e69a3b900b470d1c99d813f9de0
YUNA <yunahwang@Yunas-MacBook-Pro.local>
2019-02-28 17:33:05 -0800

    gh

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 
     $('document').ready(function(){
-      $('body').click(function(){
-        $('body').toggleClass('colorful');  
+    	var counter = 0;
+      $('button').click(function(){
+      	
+      	if(counter>5) {
+      		counter = 0;
+      	}
+
+      	var colors = ["#e3e81d", "#1de840", "#1ddde8", "#2b1de8", "#dd00f3", "black" ];
+
+        $('body').css('background', colors[counter]);  
+        counter = counter+1;
       });
     });
