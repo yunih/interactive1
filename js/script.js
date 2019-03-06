74aae049469742b4659e71ffbfbee82520596b26
YUNA <yunahwang@Yunas-MacBook-Pro.local>
2019-03-05 16:27:48 -0800

    h

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@
     	var counter = 0;
       $('button').click(function(){
       	
-      	if(counter>9) {
+      	if(counter>8) {
       		counter = 0;
       	}
 
-      	var colors = ["#e6261f", "#eb7532", "#f7d038", "#a3e048", "#49da9a","#34bbe6","#4355db","#d23be7", "black" ];
+      	var colors = ["#e6261f", "#eb7532", "#f7d038", "#a3e048", "#49da9a", "#34bbe6", "#4355db", "#d23be7", "black" ];
 
         $('body').css('background', colors[counter]);  
         counter = counter+1;
