c0410db596c0340ce481c79d1100aa3787784d51
YUNA <yunahwang@Yunas-MacBook-Pro.local>
2019-03-07 14:53:38 -0800

    h

--- sensory-experience/js/sensory.js
+++ sensory-experience/js/sensory.js
@@ -1,3 +1,18 @@
-$(document).ready(function(){
-	alert('hi'); 
+$('document').ready(function(){
+	var counter = 0;
+    $('button').click(function(){
+      	
+      	if(counter = 1) {
+      		var color = ["#06f9ff"];
+      	}
+      	 $('body').css('background', color);
+
+
+    /*$('body').click(function(){
+    	if (counter = 1) {
+    		'img' position = ['top'= '270px', 'left'='1000px']
+    	}
+    })*/
+
+     });
 });
