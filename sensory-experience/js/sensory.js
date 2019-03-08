644b5575f470717bff182f2c5647bdd283e350f6
YUNA <yunahwang@Yunas-MacBook-Pro.local>
2019-03-07 16:39:57 -0800

    h

--- sensory-experience/js/sensory.js
+++ sensory-experience/js/sensory.js
@@ -1,17 +1,18 @@
 $('document').ready(function(){
 	var counter = 0;
     $('button').click(function(){
+      	$('div').addClass('color');
       	
-      	if(counter = 1) {
-      		var color = ["#06f9ff"];
-      	}
-      	 $('body').css('background', color);
+      	
+      	 /*$('body').css('background', color);*/
 
 
     /*$('body').click(function(){
     	if (counter = 1) {
     		'img' position = ['top'= '270px', 'left'='1000px']
     	}
+      if(counter = 1) {
+          var color = ["#06f9ff"];
     })*/
 
      });
