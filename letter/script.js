ec238408254ecec72ad605b59231692764385fca
YUNA <yunahwang@Yunas-MacBook-Pro.local>
2019-05-05 15:09:34 -0700

    h

---
+++ letter/script.js
@@ -0,0 +1,20 @@
+
+$('document').ready(function(){
+
+var height = $(document).height();
+var width = $(document).width();
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var i;
+for (i = 0; i < 50; i++) { 
+	var yPos = getRandomInt(0, height);
+	var xPos = getRandomInt(0, width);  
+  $('.title').append('<div style=" top: '+yPos+'px; left: '+xPos+'px;" class="square"></div>');
+}
+
+});
